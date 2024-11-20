7fe26ad8beae15b43c1b1176d1d4377341b8aaf3
Sarschar <mahja.a.sarschar@capgemini.com>
2024-11-20 10:12:18 +0100

    add find functionality

--- src/main/java/cmd/commands/BaseCommand.java
+++ src/main/java/cmd/commands/BaseCommand.java
@@ -3,6 +3,7 @@ package cmd.commands;
 import cmd.commands.copy.CopyCommand;
 import cmd.commands.del.DelCommand;
 import cmd.commands.dir.DirCommand;
+import cmd.commands.find.FindCommand;
 import cmd.commands.mkd.MkdCommand;
 import picocli.CommandLine.Command;
 
@@ -19,7 +20,7 @@ import picocli.CommandLine.Command;
         name = "cmd",
         description = "base command",
         mixinStandardHelpOptions = true,
-        subcommands = {DirCommand.class, DelCommand.class, CopyCommand.class, MkdCommand.class})
+        subcommands = {DirCommand.class, DelCommand.class, CopyCommand.class, MkdCommand.class, FindCommand.class})
 public class BaseCommand implements Runnable {
     public BaseCommand() {
         /* intentionally empty */
