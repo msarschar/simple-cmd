8f59847ba5f78a38f218f51771541e14640d0402
foersterEric <eric.a.foerster@capgemini.com>
2024-11-20 09:19:43 +0100

    change-dir-cmd: several checks added, but spaces cause still trouble

--- src/main/java/cmd/commands/cd/ChangeCommand.java
+++ src/main/java/cmd/commands/cd/ChangeCommand.java
@@ -1,13 +1,12 @@
 package cmd.commands.cd;
 
 import cmd.SimpleCmd;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 
 @Command(
         name = "cd",
@@ -15,26 +14,43 @@ import java.nio.file.StandardCopyOption;
         mixinStandardHelpOptions = true)
 public class ChangeCommand implements Runnable {
 
-        @CommandLine.Parameters(index = "0", description = "path to navigate to")
-        private String target;
+    public static final Logger LOG = LoggerFactory.getLogger(ChangeCommand.class);
 
-        public ChangeCommand() {
-            /* intentionally empty */
+    @CommandLine.Parameters(index = "0", description = "path to navigate to")
+    private File target;
+
+    public ChangeCommand() {
+        /* intentionally empty */
+    }
+
+    @Override
+    public void run() {
+
+        if(!target.exists()){
+            LOG.error("Path '{}' does not exist. Please provide an existing path!", target);
+            return;
         }
 
-        @Override
-        public void run() {
-            try {
-                File targetFile = new File(target);
-                System.setProperty("user.dir", target);
-
-                System.out.println("Current directory: " + System.getProperty("user.dir"));
-                SimpleCmd.setCurrentLocation(targetFile);
-                System.out.println("Directory changed to: " + SimpleCmd.getCurrentLocation());
-            } catch (Exception e) {
-                System.out.println("Error: " + e.getMessage());
-                e.printStackTrace();
-            }
+        if(!target.isDirectory()){
+            LOG.error("Path '{}' is not a directory. Please provide a directory path!", target);
+            return;
         }
+
+        if(!target.isAbsolute()){
+            LOG.error("Path '{}' is not an absolute path. Please provide an absolute path!", target);
+            return;
+        }
+
+        try {
+            System.setProperty("user.dir", target.getAbsolutePath());
+
+            LOG.info("Current directory: {}", System.getProperty("user.dir"));
+            SimpleCmd.setCurrentLocation(target);
+            LOG.info("Directory changed to: " + SimpleCmd.getCurrentLocation());
+        } catch (Exception e) {
+            LOG.error("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
 }
