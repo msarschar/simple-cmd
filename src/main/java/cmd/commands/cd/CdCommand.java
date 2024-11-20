fb6521ef4704e3d2c02e344f53a9526ccf3665ea
foersterEric <eric.a.foerster@capgemini.com>
2024-11-20 09:39:20 +0100

    Changes by masarsch

--- src/main/java/cmd/commands/cd/CdCommand.java
+++ src/main/java/cmd/commands/cd/CdCommand.java
@@ -12,39 +12,45 @@ import java.io.File;
         name = "cd",
         description = "Change directory",
         mixinStandardHelpOptions = true)
-public class ChangeCommand implements Runnable {
+public class CdCommand implements Runnable {
 
-    public static final Logger LOG = LoggerFactory.getLogger(ChangeCommand.class);
+    public static final Logger LOG = LoggerFactory.getLogger(CdCommand.class);
 
     @CommandLine.Parameters(index = "0", description = "path to navigate to")
     private File target;
 
-    public ChangeCommand() {
+    public CdCommand() {
         /* intentionally empty */
     }
 
     @Override
     public void run() {
 
+        // check if the path exists
         if(!target.exists()){
             LOG.error("Path '{}' does not exist. Please provide an existing path!", target);
             return;
         }
 
+        // check if the path is a directory
         if(!target.isDirectory()){
             LOG.error("Path '{}' is not a directory. Please provide a directory path!", target);
             return;
         }
 
+        // check if the path is absolute
         if(!target.isAbsolute()){
             LOG.error("Path '{}' is not an absolute path. Please provide an absolute path!", target);
             return;
         }
 
         try {
-            System.setProperty("user.dir", target.getAbsolutePath());
 
             LOG.info("Current directory: {}", System.getProperty("user.dir"));
+            // change the current directory
+            System.setProperty("user.dir", target.getAbsolutePath());
+
+            // update the current location
             SimpleCmd.setCurrentLocation(target);
             LOG.info("Directory changed to: " + SimpleCmd.getCurrentLocation());
         } catch (Exception e) {
